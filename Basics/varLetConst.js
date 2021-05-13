952979d490530eec5110f8a27c7b1152b0f09d6e
ArthurViniciusE <arthur.alucard41@gmail.com>
2021-05-12 21:57:10 -0300

    O básico sobre Var, let e Const

---
+++ Basics/varLetConst.js
@@ -0,0 +1,5 @@
+//Prestar atenção na nomeação das variáveis
+
+var teste1 = 3 // escopo global
+let test2 = 4 // escopo de bloco
+const teste = 3 // Não é possível adicionar um novo valor, seu valor é constante.
