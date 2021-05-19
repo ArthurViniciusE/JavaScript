61154e6a27681d05d8c5729be2d5a127fe98a12e
ArthurViniciusE <arthur.alucard41@gmail.com>
2021-05-18 22:31:43 -0300

    Um pouco como funciona a linguagem JavaScript e suas chamadas e incrementos

---
+++ Basics/tipagemFraca.js
@@ -0,0 +1,13 @@
+let palavra = 'legal';
+console.log(palavra);
+console.log(typeof palavra);
+
+palavra = 3.16;
+console.log(palavra);
+console.log(typeof palavra);
+
+//Evitar nome genéricos e siglas na variáveis, exemplo;
+
+let valor = '';
+let número = 1;
+let fcb = true;
