2087ffea47d50d7848ed722c1a11922969ead338
ArthurViniciusE <arthur.alucard41@gmail.com>
2021-05-18 22:30:06 -0300

    Um pouco sobre como ter cautela ao usar números em JavaScript

---
+++ Basics/cuidadosNumber.js
@@ -0,0 +1,3 @@
+console.log(7 / 0); // Retorna tipo infinito;
+console.log("10" / 2); // Divide uma string por dois( bisonho :v), verifica conteúdo interno da string;
+console.log("10,2" / 2); // Retorna um NaN, só reconhece número inteiros na string para fazer operações;
