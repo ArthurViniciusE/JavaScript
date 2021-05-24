e31c3823bbf0bd4f076dbcb5170f5acafe038697
ArthurViniciusE <arthur.alucard41@gmail.com>
2021-05-23 21:57:37 -0300

    Um pouco de como funcionam objetos em JavaScript.

---
+++ Basics/objeto.js
@@ -0,0 +1,10 @@
+console.log(typeof Object);
+console.log(typeof new Object); // Instanciando uma função;
+
+const client = function() {};
+console.log(typeof client);
+console.log(typeof new client);
+
+class Produto {};
+console.log(typeof Produto);
+console.log(typeof new Produto());
