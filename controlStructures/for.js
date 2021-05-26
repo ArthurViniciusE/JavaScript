30d5e050d1c9f93b0014baf2ac4d9efb6ca94d06
ArthurViniciusE <arthur.alucard41@gmail.com>
2021-05-26 18:31:15 -0300

    Entendendo a estrutura for em JavaScript

---
+++ controlStructures/for.js
@@ -0,0 +1,10 @@
+
+for(let i = 1; i <= 10; i++) {
+    console.log(`Contador = ${i}`);
+}
+
+const notas = [6,5,4,7,8];
+
+for(let i =0; i < notas.length; i++){
+    console.log(`Ǹotas = ${notas[i]}`)
+}
