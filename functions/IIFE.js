e3cb6d435337573acd27635594a4646e309adba3
ArthurViniciusE <arthur.alucard41@gmail.com>
2021-05-30 19:25:34 -0300

    Estrutura de uma 'IIFE' Imediately Invoked Function Expression e para que serve.

---
+++ functions/IIFE.js
@@ -0,0 +1,7 @@
+// IIFE -->  Immediately Invoked Function Expression;
+// É um padrão usado para fugir do escopo global;
+
+(function() {
+    console.log('Será executado na hora!')
+    console.log('Foge do escopo mais abrangente!')
+})()
