a9cde208ad2e2db87c699a5c67c68a1c6d312745
ArthurViniciusE <arthur.alucard41@gmail.com>
2021-05-29 21:25:05 -0300

    tipos de declaração de função em JavaScript.

---
+++ functions/tiposDeDeclaracaoFuncao.js
@@ -0,0 +1,15 @@
+// function declaration, forma padrão de declarar;
+function soma(x,y) {
+    return x + y;
+}
+
+//function expression;
+const subtracao = function (x,y) {
+    return x - y;
+}
+
+// named function expression;
+
+const multiplicacao = function mult(x,y) {
+    return x * y;
+}
