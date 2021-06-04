99530de07f60b2e2a53c5c7b5d016281d63a0f9b
ArthurViniciusE <arthur.alucard41@gmail.com>
2021-06-04 17:39:06 -0300

    Notação literal e formas de declarar objetos a partir do ES2015.

---
+++ object/notacaoLiteral.js
@@ -0,0 +1,14 @@
+const a = 1;
+const b = 2;
+const c = 3;
+
+//Melhoria na declaração de objeto em JavaScript a partir de ES2015
+const obj = {a , b, c};
+console.log(obj);
+
+const nomeAttr = "nota";
+const valorAttr = 7.87;
+
+const obj2 = {};
+obj2[nomeAttr] = valorAttr;
+console.log(obj2);
