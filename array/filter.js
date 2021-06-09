99ad6b0580e403a95ee30a1f9eab89a4ce405593
ArthurViniciusE <arthur.alucard41@gmail.com>
2021-06-09 17:34:08 -0300

    Entendendo o método filter em array dentro do JavaScript

---
+++ array/filter.js
@@ -0,0 +1,14 @@
+const produtos = [
+    {nome: 'Notebook', preco: 2499, fragil: true},
+    {nome: 'Ipad', preco: 4199, fragil: true},
+    {nome:'Copo de Plástico', preco: 15.99, fragil: false}
+];
+
+console.log(produtos.filter(function(p) {
+    return false
+}))
+
+const caro = produto => produto.preco >= 500;
+const fragil = produto => produto.fragil;
+
+console.log(produtos.filter(caro).filter(fragil));
