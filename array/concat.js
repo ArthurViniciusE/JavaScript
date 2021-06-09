db5ad85f8b5e3d1320054ea5752bf872431ff3ed
ArthurViniciusE <arthur.alucard41@gmail.com>
2021-06-09 19:57:11 -0300

    Entendo o conceito do método concat com arrays em JavaScript.

---
+++ array/concat.js
@@ -0,0 +1,6 @@
+const filhas = ['Ana', 'Maria'];
+const filhos = ['Hagen', 'Siegrified'];
+const todos = filhas.concat(filhos, 'Alberich'); //Concatenação de dois arrays e adição de mais um elemento no parâmetro;
+console.log(todos);
+
+console.log([].concat([1,2],[3,4], 5)) // Também é possível usa-lo atavés da notação ponto;
